65b1e215a95f58d7e4d884510be047ff43485a0d
dickeyxxx <jeff@dickey.xxx>
2012-12-01 14:48:08 -0800

    sessions

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Weft::Application.config.secret_token = '6e65001648391c8a81854ce6661bcc9c3e950dbfc75d3c63f313b9846b0d89e464dd99e9a027f9fe91f60aff21bd95f130bc80aba8f56de618ea469827e6cbac'
+Weft::Application.config.secret_token = ENV['SECRET_TOKEN']
